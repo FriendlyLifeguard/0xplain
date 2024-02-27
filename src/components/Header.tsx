88dcd9391bdc3e524460c794d795f9174e4f77ea
kai zhang <kaizhi.zhang92@gmail.com>
2024-02-26 21:12:17 -0700

    add avatar dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link"
-import {memo} from "react"
+import {memo, useState} from "react"
 import Image from 'next/image'
 
 function header() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return(
   <>
     <div className='flex justify-between items-center plex-medium px-20 py-9'>
@@ -15,15 +17,34 @@ function header() {
         </div>
     </Link>
 
-    <div className="text-right text-2xl ">
+    <div className="flex items-center space-x-4">
+          {/* Avatar Icon */}
+          <div className="relative">
+            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="inline-flex items-center justify-center w-10 h-10 text-sm font-medium rounded-full">
+              <Image src="/monkey.png" alt="User Avatar" width={40} height={40} className="rounded-full" />
+            </button>
+
+          {isDropdownOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-50">
+                <Link href="/account">
+                  <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Account</div>
+                </Link>
+                <Link href="/settings">
+                  <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</div>
+                </Link>
+                <Link href="/logout">
+                  <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Log Out</div>
+                </Link>
+              </div>
+            )}
+          </div>
+
+
+    {/* <div className="text-right text-2xl "> */}
       <Link href="/createPost" className="inline-block text-white py-2 px-4 bg-emerald-500 hover:bg-emerald-600 rounded-lg shadow-md transition-colors duration-300 ease-in-out cursor-pointer" >
-        + Create Data Story
+        + Create
       </Link>
-    </div>
-    <div className="text-right text-2xl ">
-      <Link href="/createPost" className="inline-block text-white py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded-lg shadow-md transition-colors duration-300 ease-in-out cursor-pointer" >
-        + Contribute Chart
-      </Link>
+    {/* </div> */}
     </div>
  </div>
  </>
